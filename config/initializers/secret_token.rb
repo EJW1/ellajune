94f8f89643d23e72312e947bff28664ce70fbd69
Ericka Ward <ericka.ward@gmail.com>
2013-06-18 12:20:07 -0400

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ellajune::Application.config.secret_token = '951e2b0234ec398ac728b0b320ab05083d2ea11fe7ee54b5f1217b4ee20886ec74ebe7d2107b933dedf9fba98ec14ab3e0cf4b5b8149a6c9ef6e9168ed5b0f80'
