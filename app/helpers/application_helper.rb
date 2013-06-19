2d4178fbd849be417e9ca0978bad8839ec1ecdc1
Ericka Ward <ericka.ward@gmail.com>
2013-06-19 15:01:35 -0400

    post tag cloud

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def post_tag_cloud(post_tags, classes)
+    max = post_tags.sort_by(&:count).last
+    post_tags.each do |post_tag|
+      index = post_tag.count.to_f / max.count * (classes.size - 1)
+      yield(post_tag, classes[index.round])
+    end
+  end
 end
