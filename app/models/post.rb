ba66915ae61328c6f05420594429a8a9cf6cf237
Ericka Ward <ericka.ward@gmail.com>
2013-06-19 14:59:16 -0400

    post tagging methods in post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,10 @@
 class Post < ActiveRecord::Base
-  attr_accessible :content, :link, :points, :title, :user_id
+  attr_accessible :content, :link, :points, :title, :user_id, :post_tag_list
   belongs_to :user
   has_many :votes
   has_many :comments
+  has_many :post_taggings
+  has_many :post_tags, through: :post_taggings
   validates_presence_of :title
 
   extend FriendlyId
@@ -27,4 +29,25 @@ class Post < ActiveRecord::Base
     posts = Post.all
     posts.each { |post| post.update_attributes(:points => post.calc_points) }
   end
+
+  #Tagging Logic
+  def self.tagged_with(name)
+    PostTag.find_by_name!(name).posts
+  end
+
+  def self.tag_counts
+    PostTag.select("post_tags.id, post_tags.name, count(post_taggings.post_tag_id) as count").
+      joins(:post_taggings).group("post_taggings.post_tag_id, post_tags.id, post_tags.name")
+  end
+  
+  def post_tag_list
+    post_tags.map(&:name).join(", ")
+  end
+  
+  def post_tag_list=(names)
+    self.post_tags = names.split(",").map do |n|
+      PostTag.where(name: n.strip.downcase).first_or_create!
+    end
+  end
+
 end
