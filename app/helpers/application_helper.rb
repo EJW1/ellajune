41d341ad67e148b38f90851e6e0f6ac772eb572b
Ericka Ward <ericka.ward@gmail.com>
2013-06-19 15:08:56 -0400

    debugging posts tag cloud

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,14 @@
 module ApplicationHelper
-  def post_tag_cloud(post_tags, classes)
-    max = post_tags.sort_by(&:count).last
+ def post_tag_cloud(post_tags, classes)
+    max = 0
+    post_tags.each do |t|
+      if t.count.to_i > max
+        max = t.count.to_i
+      end 
+    end
     post_tags.each do |post_tag|
-      index = post_tag.count.to_f / max.count * (classes.size - 1)
+      index = post_tag.count.to_f / max * (classes.size - 1)
       yield(post_tag, classes[index.round])
     end
-  end
+  end 
 end
