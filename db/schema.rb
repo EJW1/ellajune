14a14a9aa819e831d0848108a6ac25e92426338b
Ericka Ward <ericka.ward@gmail.com>
2013-06-19 16:17:40 -0400

    generated interest tag models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130619185445) do
+ActiveRecord::Schema.define(:version => 20130619201713) do
 
   create_table "comments", :force => true do |t|
     t.text     "message"
@@ -31,6 +31,22 @@ ActiveRecord::Schema.define(:version => 20130619185445) do
   add_index "friendly_id_slugs", ["slug", "sluggable_type"], :name => "index_friendly_id_slugs_on_slug_and_sluggable_type", :unique => true
   add_index "friendly_id_slugs", ["sluggable_id"], :name => "index_friendly_id_slugs_on_sluggable_id"
   add_index "friendly_id_slugs", ["sluggable_type"], :name => "index_friendly_id_slugs_on_sluggable_type"
+
+  create_table "interest_taggings", :force => true do |t|
+    t.integer  "interest_tag_id"
+    t.integer  "user_id"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+  end
+
+  add_index "interest_taggings", ["interest_tag_id"], :name => "index_interest_taggings_on_interest_tag_id"
+  add_index "interest_taggings", ["user_id"], :name => "index_interest_taggings_on_user_id"
+
+  create_table "interest_tags", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "post_taggings", :force => true do |t|
     t.integer  "post_tag_id"
