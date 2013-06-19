1ec031776e32b058dbc859a77f39ac3c221cff3f
Ericka Ward <ericka.ward@gmail.com>
2013-06-19 15:53:40 -0400

    starting interest_tag implementation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,5 +10,18 @@ module ApplicationHelper
       index = post_tag.count.to_f / max * (classes.size - 1)
       yield(post_tag, classes[index.round])
     end
-  end 
+  end
+
+  def interest_tag_cloud(interest_tags, classes)
+    max = 0
+    interest_tags.each do |t|
+      if t.count.to_i > max
+        max = t.count.to_i
+      end 
+    end
+    interest_tags.each do |post_tag|
+      index = interest_tag.count.to_f / max * (classes.size - 1)
+      yield(interest_tag, classes[index.round])
+    end
+  end  
 end
