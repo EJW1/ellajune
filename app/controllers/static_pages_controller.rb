067a17844ec193923d8cf3c6bce20bf0846dc8c9
Ericka Ward <ericka.ward@gmail.com>
2013-06-24 14:47:25 -0400

    before filter for static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,14 +1,16 @@
 class StaticPagesController < ApplicationController
+  before_filter :authenticate_user!, :except => [:about]
+
   def community_guidelines
   end
 
-  def welcome
+  def about
   end
 
   def get_involved
   end
 
-  def contact
+  def staff
   end
 
   def vision
