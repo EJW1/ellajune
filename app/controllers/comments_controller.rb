4922e1aa9d6472109241b6f9d7024e73293447e8
Ericka Ward <ericka.ward@gmail.com>
2013-07-08 13:00:25 -0400

    updated user will paginate

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,7 @@ before_filter :authenticate_user!
   # GET /comments
   # GET /comments.json
   def index
-    @comments = Comment.all
+    @comments = Comment.paginate(:page => params[:page], :per_page => 30)
 
     respond_to do |format|
       format.html # index.html.erb
