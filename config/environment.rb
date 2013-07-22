4527e740df8059e7d07d4726aac7dfe61a315e73
Ericka Ward <ericka.ward@gmail.com>
2013-07-22 13:07:38 -0400

    see if sendgrid is working

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Ellajune::Application.initialize!
+
+#Sendgrid configuration
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['app16378421@heroku.com'],
+  :password       => ENV['zdhfew4d'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
